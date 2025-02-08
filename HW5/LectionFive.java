6ea44d680018a58e302e67660f39631389a59e24
Olha Korinchevska <olga.korinchevskaya@aventus.work>
2025-02-09 00:29:01 +0200

    Added homework number 5

---
+++ HW5/LectionFive.java
@@ -0,0 +1,18 @@
+public class LectionFive {
+
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+
+        if (!weekday || vacation) {
+            return true; // Спим, если не рабочий день или отпуск
+        } else {
+            return false; // Не спим, если рабочий день и нет отпуска
+        }
+    }
+    public static void main(String[] args) {
+        // Тесты
+        System.out.println(sleepIn(false, false)); // Ожидаем true
+        System.out.println(sleepIn(true, false));  // Ожидаем false
+        System.out.println(sleepIn(false, true));  // Ожидаем true
+    }
+
+}
