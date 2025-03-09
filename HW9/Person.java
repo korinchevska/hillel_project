c5d171c81e4996c1e1887e567121483c791b984c
Olha Korinchevska <olga.korinchevskaya@aventus.work>
2025-03-09 21:59:11 +0200

    Revert "ДЗ 11.1. Програма Person - заключна частина"
    This reverts commit 23f5e6738f0291c8a3441659de4e70fc07a050ab.

--- HW9/Person.java
+++ HW9/Person.java
@@ -1,7 +1,7 @@
 //import java.util.Date;
 import java.time.Year;
 
-abstract class Person implements Displayable{
+public class Person implements Displayable{
     String name;
     //Date dob;
     Profession profession;
@@ -36,14 +36,8 @@ abstract class Person implements Displayable{
     public int getBirthYear() {
         return year;
     }
-    public abstract void displayInfo();
+    public void displayInfo (){
+        System.out.println("Name: " + name + ", Age: " + getAge() + ", Profession: " + profession);
 
-   /* public void displayInfo(boolean showBirthYear) {
-        if (showBirthYear) {
-            System.out.println("Ім'я: " + name + ", Рік народження: " + getAge() + ", Професія: " + profession);
-        } else {
-            displayInfo(); // Викликаємо стандартний варіант
-        }*/
-    //System.out.println("Name: " + name + ", Age: " + getAge() + ", Profession: " + profession);
-
+    }
     }
